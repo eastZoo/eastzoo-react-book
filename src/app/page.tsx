eaa7c31218caf62a0e35b99f3f264feb70ab0de0
동주 <dongju010@insystem.kr>
2024-02-05 14:37:10 +0900

    UPDATE : 테스트용 라인 삭제

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,3 @@
-"use client";
-import CommaConverter from "@/lib/utils/commaCoverter";
-import StringDateConverter from "@/lib/utils/stringDateCoverter";
-import { useState } from "react";
-
 export default function Home() {
-  const [state, setState] = useState(CommaConverter("1232130"));
-  const now = new Date();
-
-  console.log(now);
-  return (
-    <main>
-      <div>예산</div>
-      <div>{state}</div>
-      <div>{StringDateConverter(now)}</div>
-    </main>
-  );
+  return <main>테스트</main>;
 }
