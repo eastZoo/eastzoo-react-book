f23b0a97ffa7f0a612be4a7b62a96ae42f891f0d
동주 <dongju010@insystem.kr>
2024-02-06 11:19:33 +0900

    FEAT : ErrorBoundary 추가, 각 useQuery에대한 메인에서 통합 에러처리 작성중

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,20 @@
 "use client";
-
 import { RecoilRoot } from "recoil";
 import StyledComponentsRegistry from "@/lib/styled-components.-registry";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {
+  QueryCache,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
 import { ThemeProvider } from "styled-components";
 import { theme } from "@/styles/theme";
 import { GlobalStyle } from "@/styles/globals-style";
 import Head from "./head";
 import AuthGuard from "./authGuard";
+import ErrorBoundary from "@/error/errorBoundary";
+import { queryCacheOnError } from "@/error/errorState";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
 
 export default function RootLayout({
   children,
@@ -21,9 +28,12 @@ export default function RootLayout({
         retry: 1,
         refetchOnMount: true,
         refetchOnWindowFocus: true,
-        staleTime: 1000 * 10,
+        staleTime: 1000 * 1000,
       },
     },
+    queryCache: new QueryCache({
+      onError: (error, query) => queryCacheOnError(error, query),
+    }),
   });
 
   return (
@@ -33,15 +43,18 @@ export default function RootLayout({
         <ThemeProvider theme={theme}>
           <body>
             <AuthGuard>
-              <QueryClientProvider client={queryClient}>
-                <StyledComponentsRegistry>
-                  <>
-                    <GlobalStyle />
-                    {children}
-                  </>
-                </StyledComponentsRegistry>
-              </QueryClientProvider>
+              <ErrorBoundary>
+                <QueryClientProvider client={queryClient}>
+                  <StyledComponentsRegistry>
+                    <>
+                      <GlobalStyle />
+                      {children}
+                    </>
+                  </StyledComponentsRegistry>
+                </QueryClientProvider>
+              </ErrorBoundary>
             </AuthGuard>
+            <ToastContainer />
           </body>
         </ThemeProvider>
       </RecoilRoot>
--- src/lib/utils/stringDateCoverter.ts
+++ src/lib/utils/stringDateCoverter.ts
@@ -6,19 +6,20 @@
  */
 export default function StringDateConverter(
   inputDate: string | Date,
-  lastUnit: string = "full"
+  lastType: string = "full",
+  unit: string
 ) {
   const date = new Date(inputDate);
 
   const year = date.getFullYear();
   const month = (date.getMonth() + 1).toString().padStart(2, "0");
-  if (lastUnit == "m") return `${year}.${month}`;
+  if (lastType == "m") return `${year}.${month}`;
 
   const day = date.getDate().toString().padStart(2, "0");
-  if (lastUnit == "d") return `${year}.${month}.${day}`;
+  if (lastType == "d") return `${year}.${month}.${day}`;
 
   const hour = date.getHours().toString().padStart(2, "0");
-  if (lastUnit == "h") return `${year}.${month}.${day}.${hour}`;
+  if (lastType == "h") return `${year}.${month}.${day}.${hour}`;
 
   const minute = date.getMinutes().toString().padStart(2, "0");
   return `${year}년 ${month}월 ${day}일 ${hour}시 ${minute}분`;
